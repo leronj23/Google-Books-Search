87be02ac546356a3d34bf22f70c6b3f8e80fd5f6
LeRon Jackson <leronj23@gmail.com>
2019-04-13 12:17:42 -0400

    initial commit

---
+++ client/build/precache-manifest.56864bb33676f26b03c439a283864557.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da52a51ba95bf5ff8dd2",
+    "url": "/static/css/main.44905168.chunk.css"
+  },
+  {
+    "revision": "da52a51ba95bf5ff8dd2",
+    "url": "/static/js/main.6b5ec6ab.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "70647b92499084d59e36",
+    "url": "/static/js/2.b16cdd1e.chunk.js"
+  },
+  {
+    "revision": "e2677b75fc8fd44872e7b43fb81693a9",
+    "url": "/index.html"
+  }
+];
